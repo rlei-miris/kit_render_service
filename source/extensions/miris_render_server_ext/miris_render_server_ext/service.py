f53d9676c38efcaf10f472a7635cfbec40eb8207
rlei-miris <r.lei@miris.com>
2025-08-21 14:12:36 -0700

    Block execution until renders are complete.  Return a response with the image paths

--- source/extensions/miris_render_server_ext/miris_render_server_ext/service.py
+++ source/extensions/miris_render_server_ext/miris_render_server_ext/service.py
@@ -8,6 +8,8 @@
 # without an express license agreement from NVIDIA CORPORATION or
 # its affiliates is strictly prohibited.
 
+import os
+import tempfile
 from pathlib import Path
 from pydantic import BaseModel, Field
 from typing import Tuple
@@ -22,7 +24,7 @@ from pxr import UsdGeom
 router = ServiceAPIRouter(tags=["Miris Render Server Extension"])
 
 
-class OpenStageModel(BaseModel):
+class OpenStageRequestData(BaseModel):
     usd_file_location: str = Field(
         default="/tmp/scene.usd",
         title="USD File Location",
@@ -35,22 +37,22 @@ class OpenStageModel(BaseModel):
     summary="Open a USD file",
     description="An endpoint to open a USD file as the active stage",
 )
-async def open_stage(open_stage_data: OpenStageModel):
+async def open_stage(request_data: OpenStageRequestData):
     print("[miris_render_server_ext] /open_stage was called")
 
     # Open the usd file
     usd_context = omni.usd.get_context()
-    usd_context.open_stage(open_stage_data.usd_file_location)
+    usd_context.open_stage(request_data.usd_file_location)
 
     # Set to path traced
     rep.settings.set_render_pathtraced()
 
-    msg = f"[miris_render_server_ext] Opened stage: {open_stage_data.usd_file_location}"
+    msg = f"[miris_render_server_ext] Opened stage: {request_data.usd_file_location}"
     print(msg)
     return msg
 
 
-class RenderModel(BaseModel):
+class RenderRequestData(BaseModel):
 
     camera_name: str = Field(
         default="camera_0",
@@ -89,27 +91,35 @@ class RenderModel(BaseModel):
     )
 
 
+class RenderResponseData(BaseModel):
+
+    color_image_path: str = Field(title="Output color image path")
+    depth_image_path: str = Field(title="Output depth image path")
+    depth_npy_path: str = Field(title="Output depth npy path")
+
+
 @router.post(
     "/render",
     summary="Render from a camera position",
     description="An endpoint render from a camera position",
+    response_model=RenderResponseData,
 )
-async def render(render_data: RenderModel):
-    print(f"[miris_render_server_ext] /render was called with args: {render_data}")
+async def render(request_data: RenderRequestData):
+    print(f"[miris_render_server_ext] /render was called with args: {request_data}")
 
     # Create the hydra render product
     camera = rep.create.camera(
-        position=render_data.camera_position,
-        rotation=render_data.camera_rotation,
-        focal_length=render_data.camera_focal_length,
-        horizontal_aperture=render_data.camera_horizontal_aperture,
-        name=render_data.camera_name,
+        position=request_data.camera_position,
+        rotation=request_data.camera_rotation,
+        focal_length=request_data.camera_focal_length,
+        horizontal_aperture=request_data.camera_horizontal_aperture,
+        name=request_data.camera_name,
     )
 
     render_product = rep.create.render_product(
         camera,
-        resolution=render_data.image_resolution,
-        name=render_data.camera_name,
+        resolution=request_data.image_resolution,
+        name=request_data.camera_name,
     )
 
     with rep.trigger.on_frame(max_execs=1):
@@ -117,17 +127,30 @@ async def render(render_data: RenderModel):
 
     # Initialize and attach writer
     writer = rep.WriterRegistry.get("BasicWriter")
+    tmp_dir = tempfile.mkdtemp()
+    output_dir = os.path.join(tmp_dir, f"output_{request_data.camera_name}")
     writer.initialize(
-        output_dir=f"_output_{render_data.camera_name}",
+        output_dir=output_dir,
         rgb=True,
         distance_to_image_plane=True,
         colorize_depth=True,
     )
+
     writer.attach([render_product])
 
-    # Execute the tasks asynchronously
-    rep.orchestrator.run()
+    # Execute the tasks, wait for completion
+    await rep.orchestrator.run_until_complete_async()
 
-    msg = f"[miris_render_server_ext] Rendered camera {render_data.camera_name}"
-    print(msg)
-    return msg
+    # Collect artifacts
+    # TODO: Is there a way to query where the Replicator writer is saving the files?
+    color_image_path = os.path.join(output_dir, "rgb_0000.png")
+    depth_image_path = os.path.join(output_dir, "distance_to_image_plane_0000.png")
+    depth_npy_path = os.path.join(output_dir, "distance_to_image_plane_0000.npy")
+    for file_path in (color_image_path, depth_image_path, depth_npy_path):
+        assert os.path.isfile(file_path), f"Expected {file_path} to exist"
+
+    return RenderResponseData(
+        color_image_path=color_image_path,
+        depth_image_path=depth_image_path,
+        depth_npy_path=depth_npy_path,
+    )
